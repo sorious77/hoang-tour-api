568726cbd338c8a4da08aac4ba5831d71342b0ab
sorious77 <sorious77@naver.com>
2024-08-24 17:40:29 +0900

    :tada: 프로젝트 생성

---
+++ src/main/kotlin/com/hoang/hoangtourapi/HoangTourApiApplication.kt
@@ -0,0 +1,11 @@
+package com.hoang.hoangtourapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HoangTourApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<HoangTourApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/hoang/hoangtourapi/HoangTourApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hoang.hoangtourapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HoangTourApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

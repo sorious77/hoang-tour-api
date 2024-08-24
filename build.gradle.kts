9aa384c7d5aec3aa1d6a1abe49fb67d91952cf1d
sorious77 <sorious77@naver.com>
2024-08-24 18:02:24 +0900

    :sparkles: Swagger 및 H2 DB 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
+    // H2
     runtimeOnly("com.h2database:h2")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ src/main/kotlin/com/hoang/hoangtourapi/model/Member.kt
@@ -0,0 +1,15 @@
+package com.hoang.hoangtourapi.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val memberId: Long,
+    val name: String,
+    val email: String
+)
